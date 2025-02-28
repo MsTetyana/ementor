676162be6a886fabb3230b2c9f2537d3245c30e6
MsTetyana <tetanaivasuk246@gmail.com>
2025-02-28 13:24:22 +0200

    burger menu done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+// accordeon start
+
 document.querySelectorAll('.accordeon__triger').forEach((item) => {
   item.addEventListener('click', () => {
     item.parentNode.classList.toggle('accordeon__item--active')
   })
 });
+
+// accordeon end
+
+
+// burger start
+const menuBtn = document.querySelector('.menu__btn');
+const menuList = document.querySelector('.menu__list');
+
+menuBtn.addEventListener('click', () => {
+  menuList.classList.toggle('menu__list--active')
+})
+// burger end
